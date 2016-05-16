2a0d7e5f4bcd43a19154e9d87c64e4d54f6bca25
zafeingo <zach_feingold@hotmail.com>
2016-05-15 22:40:09 -0700

    Settings/Landing Page complete, History UI in progress

---
+++ Dietskan/HistoryUIViewController.m
@@ -0,0 +1,37 @@
+//
+//  HistoryUIViewController.m
+//  Dietskan
+//
+//  Created by Zach Feingold on 5/15/16.
+//  Copyright © 2016 DietSkan. All rights reserved.
+//
+
+#import "HistoryUIViewController.h"
+
+@interface HistoryUIViewController ()
+
+@end
+
+@implementation HistoryUIViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
