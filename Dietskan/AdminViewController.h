0a110012ee43db22f2a2af0dbb32bf0aad07dc40
dietskan <dietskan@dietskans-macbook-pro.local>
2016-05-29 17:39:14 -0700

    Admin password change UI

---
+++ Dietskan/AdminViewController.h
@@ -0,0 +1,13 @@
+//
+//  AdminViewController.h
+//  Dietskan
+//
+//  Created by dietskan on 5/29/16.
+//  Copyright © 2016 DietSkan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AdminViewController : UIViewController
+
+@end
